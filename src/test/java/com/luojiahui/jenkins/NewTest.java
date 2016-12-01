326804cced126ea8bff8ba8dc899c46aa52c01c7
air <air@air-PC>
2016-12-01 19:27:48 +0800

    new item

---
+++ src/test/java/com/luojiahui/jenkins/NewTest.java
@@ -0,0 +1,18 @@
+package com.luojiahui.jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test(groups="bbb")
+  public void f() {
+	  System.out.println("f");
+	  Reporter.log("f");
+  }
+  
+  @Test(groups={"aaa"})
+  public void g(){
+	  System.out.println("g");
+	  Reporter.log("g");
+  }
+}
